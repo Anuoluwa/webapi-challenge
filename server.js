e0bd3dd2765ee857a83c079b61f0b95fb1791be7
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-07-12 08:27:53 +0100

    configure babel with .babelrc file

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+// import routes from './api'
+// import logger from './middlewares/logger'
+
+
+const server = express();
+server.use(express.json());
+// server.use(logger);
+
+// server.use('/api', routes);
+
+server.get('/', (req, res) => {
+  res.send(`<h2>Welcome to Projects & Actions API</h2>`)
+});
+
+
+server.all('*', (req, res) => {
+  res.json(`
+    Sorry, invalid routes, try again!
+  `);
+});
+
+export default server;
